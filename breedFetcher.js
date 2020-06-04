b1cb4478fe44d9078743c2bf6e6c7e254105fbb0
Brian Tran <98brian.tran@gmail.com>
2020-06-04 22:53:50 +0000

    Finished breedfetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,16 +3,18 @@ const request = require('request');
 const fetchBreedDescription = (breedName, callback) => {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (err, response, body) => {
     if (err) {
-      callback(err);
+      let error = "No breed found or no breed entered";
+      callback(error, null);
     }
+
     const data = JSON.parse(body);
-    
     if (data[0]) {
       callback(null, data[0].description);
     } else {
-      callback(err);
+      let error = "No breed found or no breed entered";
+      callback(error, null);
     }
   });
 };
 
-module.exports = fetchBreedDescription;
+module.exports = { fetchBreedDescription };
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const fetchBreedDescription = require('./breedFetcher');
-const breed = process.argv[2];
+const { fetchBreedDescription } = require('./breedFetcher');
 
-fetchBreedDescription(breed, (error, desc) => {
+const breedName = process.argv[2];
+
+fetchBreedDescription(breedName, (error, desc) => {
   if (error) {
     console.log('Error fetch details:', error);
   } else {
-    return desc;
+    console.log(desc);
   }
 });
