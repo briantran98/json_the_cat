1fa2c3fd10426ffb591e557fbff114b0fe1ca9ef
Brian Tran <98brian.tran@gmail.com>
2020-06-04 21:29:53 +0000

    Refactored code

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,18 @@
 const request = require('request');
-const arg = process.argv.slice(2);
 
-const breedFetch = (breed) => {
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (err, response, body) => {
+const fetchBreedDescription = (breedName, callback) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (err, response, body) => {
     if (err) {
-      throw `Error looking for breed: ${err}`;
+      callback(err);
     }
     const data = JSON.parse(body);
-
+    
     if (data[0]) {
-      console.log(data[0].description);
+      callback(null, data[0].description);
     } else {
-      throw `Error no breed inputted: ${err}\nPlease input a breed to fetch.`;
+      callback(err);
     }
   });
-};
+}
 
-breedFetch(arg[0]);
+module.exports = fetchBreedDescription;
